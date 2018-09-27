50e738d820215d481d3e8a0808688288453c78e9
Tsundere Chen <tsundere@kisekinoashita.org>
2018-09-27 19:21:34 +0800

    Change driver.py to be compatible with Python 3
    scripts/driver.py can't run using Python 3 due to print function.
    
    This commit change print to print(), which works with both Python 2 and 3,
    so there should be no issue running whether your Python version is.

--- scripts/driver.py
+++ scripts/driver.py
@@ -59,7 +59,7 @@ class Tracer:
 
     def runTrace(self, tid):
         if not tid in self.traceDict:
-            print "ERROR: No trace with id %d" % tid
+            print("ERROR: No trace with id %d" % tid)
             return False
         fname = "%s/%s.cmd" % (self.traceDirectory, self.traceDict[tid])
         vname = "%d" % self.verbLevel
@@ -67,18 +67,18 @@ class Tracer:
         try:
             retcode = subprocess.call(clist)
         except Exception as e:
-            print "Call of '%s' failed: %s" % (" ".join(clist), e)
+            print("Call of '%s' failed: %s" % (" ".join(clist), e))
             return False
         return retcode == 0
 
     def run(self, tid = 0):
         scoreDict = { k : 0 for k in self.traceDict.keys() }
-        print "---\tTrace\t\tPoints"
+        print("---\tTrace\t\tPoints")
         if tid == 0:
             tidList = self.traceDict.keys()
         else:
             if not tid in self.traceDict:
-                print "ERROR: Invalid trace ID %d" % tid
+                print("ERROR: Invalid trace ID %d" % tid)
                 return
             tidList = [tid]
         score = 0
@@ -86,15 +86,15 @@ class Tracer:
         for t in tidList:
             tname = self.traceDict[t]
             if self.verbLevel > 0:
-                print "+++ TESTING trace %s:" % tname
+                print("+++ TESTING trace %s:" % tname)
             ok = self.runTrace(t)
             maxval = self.maxScores[t]
             tval = maxval if ok else 0
-            print "---\t%s\t%d/%d" % (tname, tval, maxval)
+            print("---\t%s\t%d/%d" % (tname, tval, maxval))
             score += tval
             maxscore += maxval
             scoreDict[t] = tval
-        print "---\tTOTAL\t\t%d/%d" % (score, maxscore)
+        print("---\tTOTAL\t\t%d/%d" % (score, maxscore))
         if self.autograde:
             # Generate JSON string
             jstring = '{"scores": {'
@@ -105,14 +105,14 @@ class Tracer:
                 first = False
                 jstring += '"%s" : %d' % (self.traceProbs[k], scoreDict[k])
             jstring += '}}'
-            print jstring
+            print(jstring)
 
 def usage(name):
-    print "Usage: %s [-h] [-p PROG] [-t TID] [-v VLEVEL]" % name
-    print "  -h        Print this message"
-    print "  -p PROG   Program to test"
-    print "  -t TID    Trace ID to test"
-    print "  -v VLEVEL Set verbosity level (0-3)"
+    print("Usage: %s [-h] [-p PROG] [-t TID] [-v VLEVEL]" % name)
+    print("  -h        Print this message")
+    print("  -p PROG   Program to test")
+    print("  -t TID    Trace ID to test")
+    print("  -v VLEVEL Set verbosity level (0-3)")
     sys.exit(0)
 
 def run(name, args):
@@ -137,7 +137,7 @@ def run(name, args):
         elif opt == '-A':
             autograde = True
         else:
-            print "Unrecognized option '%s'" % opt
+            print("Unrecognized option '%s'" % opt)
             usage(name)
     if not levelFixed and autograde:
         vlevel = 0
