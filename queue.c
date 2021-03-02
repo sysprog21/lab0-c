a9d5654717ae16a8f65250538a3b2b1961ea3f9a
wiasliaw <leewei48750@gmail.com>
2021-03-03 07:47:59 +0800

    Feat(queue.[ch]): q_size
    add `size` field in queue_t to fetch queue size in O(1)

--- queue.c
+++ queue.c
@@ -81,10 +81,7 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  */
 int q_size(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+    return q->size;
 }
 
 /*
