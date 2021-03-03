ff8d6b2ebcf220b2c57e219c2cef3be9268faf57
wiasliaw <leewei48750@gmail.com>
2021-03-04 02:22:41 +0800

    Feat(queue.c): q_new
    handle error if malloc fail and return NULL

--- queue.c
+++ queue.c
@@ -12,7 +12,8 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
+    if (!q)
+        return NULL;
     q->head = NULL;
     return q;
 }
