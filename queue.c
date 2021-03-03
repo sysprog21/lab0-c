c877701120cd4a292e319959fe19ec7a9159f8a7
wiasliaw <leewei48750@gmail.com>
2021-03-04 03:22:44 +0800

    Feat(queue.c): q_insert_head
    init a new element according to char *s and insert into queue's head

--- queue.c
+++ queue.c
@@ -35,11 +35,29 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    // if q is NULL, return false
+    if (!q)
+        return false;
+
+    // create a element
+    // return false if malloc fail
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if (!newh)
+        return false;
+
+    // create a string
+    char *temp = malloc(sizeof(char) * (strlen(s) + 1));
+    if (!temp) {
+        free(newh);
+        return false;
+    }
+
+    // copy string
+    newh->value = temp;
+    strncpy(newh->value, s, (strlen(s) + 1));
+
+    // insert element to head
     newh->next = q->head;
     q->head = newh;
     return true;
