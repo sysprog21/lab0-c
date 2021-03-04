e6d87d557978e2058ac57e5e216b6e27904e1237
wiasliaw <leewei48750@gmail.com>
2021-03-05 02:27:33 +0800

    Feat(queue.c): q_free
    free all element in queue and queue itself

--- queue.c
+++ queue.c
@@ -22,8 +22,15 @@ queue_t *q_new()
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
-    /* Free queue structure */
+    // if q is NULL, return false
+    if (!q)
+        return;
+    while (q->head != NULL) {
+        list_ele_t *temp = q->head;
+        q->head = temp->next;
+        free(temp->value);
+        free(temp);
+    }
     free(q);
 }
 
