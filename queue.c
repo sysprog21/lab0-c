392750ac855e388cfc7f4e8e30e979ddb04af566
jim12312321 <jim12312321@gmail.com>
2022-02-16 23:36:42 +0800

    Add q_size
    add q_size function in queue.c.
    use code via "https://hackmd.io/@sysprog/linux2022-lab0#%E7%89%9B%E5%88%80%E5%B0%8F%E8%A9%A6".

--- queue.c
+++ queue.c
@@ -91,7 +91,15 @@ void q_release_element(element_t *e)
  */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *li;
+
+    list_for_each (li, head)
+        len++;
+    return len;
 }
 
 /*
