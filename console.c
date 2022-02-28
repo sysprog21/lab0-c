ffabd9fa7a786fe4431087ad8751b96880664d22
eecheng87 <yucheng871011@gmail.com>
2022-02-28 13:20:43 +0800

    Fix accessing NULL pointer
    Close #81

--- console.c
+++ console.c
@@ -650,7 +650,7 @@ bool run_console(char *infile_name)
             linenoiseHistoryAdd(cmdline);       /* Add to the history. */
             linenoiseHistorySave(HISTORY_FILE); /* Save the history on disk. */
             linenoiseFree(cmdline);
-            while (buf_stack->fd != STDIN_FILENO)
+            while (buf_stack && buf_stack->fd != STDIN_FILENO)
                 cmd_select(0, NULL, NULL, NULL, NULL);
             has_infile = false;
         }
