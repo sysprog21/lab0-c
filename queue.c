36dbf1d85c29694d41e6e5257d6fdbcba2344a40
Middle <fgh669988@gmail.com>
2022-07-03 21:51:38 +0900

    Add linux list reference other's code

--- queue.c
+++ queue.c
@@ -304,7 +304,7 @@ struct list_head *split(struct list_head *head)
     return tmp;
 }
 
-struct list_head *merge(struct list_head *first, struct list_head *second)
+struct list_head *merge1(struct list_head *first, struct list_head *second)
 {
     if (!second)
         return first;
@@ -316,13 +316,13 @@ struct list_head *merge(struct list_head *first, struct list_head *second)
     element_t *ele_second = container_of(second, element_t, list);
 
     if (strcmp(ele_first->value, ele_second->value) < 0) {
-        first->next = merge(first->next, second);
+        first->next = merge1(first->next, second);
         // first->next->prev = first;
         // first->prev = NULL;
         // printf("first");
         return first;
     } else {
-        second->next = merge(first, second->next);
+        second->next = merge1(first, second->next);
         // second->next->prev = second;
         // second->prev = NULL;
         // printf("second");
@@ -341,7 +341,188 @@ struct list_head *merge_sort(struct list_head *head)
     head = merge_sort(head);
     second = merge_sort(second);
 
-    return merge(head, second);
+    return merge1(head, second);
+}
+
+
+typedef unsigned char u8;
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+typedef int
+    __attribute__((nonnull(2, 3))) (*list_cmp_func_t)(void *,
+                                                      const struct list_head *,
+                                                      const struct list_head *);
+int sort_comp(void *p, const struct list_head *a, const struct list_head *b)
+{
+    return strcmp(list_entry(a, element_t, list)->value,
+                  list_entry(b, element_t, list)->value);
+}
+
+
+__attribute__((nonnull(2, 3, 4))) static struct list_head *
+merge(void *priv, list_cmp_func_t cmp, struct list_head *a, struct list_head *b)
+{
+    struct list_head *head = NULL, **tail = &head;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (cmp(priv, a, b) <= 0) {
+            *tail = a;
+            tail = &a->next;
+            a = a->next;
+            if (!a) {
+                *tail = b;
+                break;
+            }
+        } else {
+            *tail = b;
+            tail = &b->next;
+            b = b->next;
+            if (!b) {
+                *tail = a;
+                break;
+            }
+        }
+    }
+    return head;
+}
+
+__attribute__((nonnull(2, 3, 4, 5))) static void merge_final(
+    void *priv,
+    list_cmp_func_t cmp,
+    struct list_head *head,
+    struct list_head *a,
+    struct list_head *b)
+{
+    struct list_head *tail = head;
+    u8 count = 0;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (cmp(priv, a, b) <= 0) {
+            tail->next = a;
+            a->prev = tail;
+            tail = a;
+            a = a->next;
+            if (!a)
+                break;
+        } else {
+            tail->next = b;
+            b->prev = tail;
+            tail = b;
+            b = b->next;
+            if (!b) {
+                b = a;
+                break;
+            }
+        }
+    }
+
+    /* Finish linking remainder of list b on to tail */
+    tail->next = b;
+    do {
+        /*
+         *       * If the merge is highly unbalanced (e.g. the input is
+         *               * already sorted), this loop may run many iterations.
+         *                       * Continue callbacks to the client even though
+         * no
+         *                               * element comparison is needed, so the
+         * client's cmp()
+         *                                       * routine can invoke
+         * cond_resched() periodically.
+         *                                               */
+        if (unlikely(!++count))
+            cmp(priv, b, b);
+        b->prev = tail;
+        tail = b;
+        b = b->next;
+    } while (b);
+
+    /* And the final links to make a circular doubly-linked list */
+    tail->next = head;
+    head->prev = tail;
+}
+
+__attribute__((nonnull(2, 3))) void list_sort(void *priv,
+                                              struct list_head *head,
+                                              list_cmp_func_t cmp)
+{
+    struct list_head *list = head->next, *pending = NULL;
+    size_t count = 0; /* Count of pending */
+
+    if (list == head->prev) /* Zero or one elements */
+        return;
+
+    /* Convert to a null-terminated singly-linked list. */
+    head->prev->next = NULL;
+
+    /*
+     *   * Data structure invariants:
+     *       * - All lists are singly linked and null-terminated; prev
+     *           *   pointers are not maintained.
+     *               * - pending is a prev-linked "list of lists" of sorted
+     *                   *   sublists awaiting further merging.
+     *                       * - Each of the sorted sublists is power-of-two in
+     * size.
+     *                           * - Sublists are sorted by size and age,
+     * smallest & newest at front.
+     *                               * - There are zero to two sublists of each
+     * size.
+     *                                   * - A pair of pending sublists are
+     * merged as soon as the number
+     *                                       *   of following pending elements
+     * equals their size (i.e.
+     *                                           *   each time count reaches an
+     * odd multiple of that size).
+     *                                               *   That ensures each later
+     * final merge will be at worst 2:1.
+     *                                                   * - Each round consists
+     * of:
+     *                                                       *   - Merging the
+     * two sublists selected by the highest bit
+     *                                                           *     which
+     * flips when count is incremented, and
+     *                                                               *   -
+     * Adding an element from the input as a size-1 sublist.
+     *                                                                   */
+    do {
+        size_t bits;
+        struct list_head **tail = &pending;
+
+        /* Find the least-significant clear bit in count */
+        for (bits = count; bits & 1; bits >>= 1)
+            tail = &(*tail)->prev;
+        /* Do the indicated merge */
+        if (likely(bits)) {
+            struct list_head *a = *tail, *b = a->prev;
+
+            a = merge(priv, cmp, b, a);
+            /* Install the merged result in place of the inputs */
+            a->prev = b->prev;
+            *tail = a;
+        }
+
+        /* Move one element from input list to pending */
+        list->prev = pending;
+        pending = list;
+        list = list->next;
+        pending->next = NULL;
+        count++;
+    } while (list);
+
+    /* End of input; merge together all the pending lists. */
+    list = pending;
+    pending = pending->prev;
+    for (;;) {
+        struct list_head *next = pending->prev;
+
+        if (!next)
+            break;
+        list = merge(priv, cmp, pending, list);
+        pending = next;
+    }
+    /* The final merge, rebuilding prev links */
+    merge_final(priv, cmp, head, pending, list);
 }
 
 /* Sort elements of queue in ascending order */
@@ -350,18 +531,18 @@ void q_sort(struct list_head *head)
     if (head == NULL || head->next == head) {
         return;
     }
-    struct list_head *list = head->next;
-    head->prev->next = NULL;
-    list = merge_sort(head->next);
-    head->next = list;
+    //    struct list_head *list = head->next;
+    //    head->prev->next = NULL;
+    //    list = merge_sort(list);
+    //   head->next = list;
 
-    struct list_head *i = head;
-    while (i->next != NULL) {
-        i->next->prev = i;
-        i = i->next;
-    }
-    head->prev = i;
-    i->next = head;
+    // struct list_head *i = head;
+    // while (i->next != NULL) {
+    //     i->next->prev = i;
+    //     i = i->next;
+    // }
+    // head->prev = i;
+    // i->next = head;
+    //
+    list_sort(NULL, head, sort_comp);
 }
-
-
