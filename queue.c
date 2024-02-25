8d628913605bed244b33d67337c388ae015fa711
jimmylu890303 <jimmylu9460@gmail.com>
2024-02-25 20:36:50 +0800

    Implement q_free function
    In the implementation of the queue, linked lists are connected using
    the `list_head` structure defined within the element_t. Thus, it's possible
    to traverse all element_t in the queue using list_for_each_entry_safe defined
    in list.h, obtaining the address of each element_t to release its memory.
    Additionally, since the memory block pointed to by the string within element_t
    also needs to be released, finally, the memory block pointed to by head's list_head
    needs to be freed as well.

--- queue.c
+++ queue.c
@@ -22,7 +22,18 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (!l)
+        return;
+    element_t *cur, *safe = NULL;
+    list_for_each_entry_safe (cur, safe, l, list) {
+        if (cur->value)
+            free(cur->value);
+        free(cur);
+    }
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
