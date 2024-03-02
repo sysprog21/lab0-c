e81d59751448f6ec3ec9155745d0428d32f2b686
jimmylu890303 <jimmylu9460@gmail.com>
2024-03-02 23:55:39 +0800

    Add q_shuffle command to qtest

--- qtest.c
+++ qtest.c
@@ -57,6 +57,8 @@ extern int show_entropy;
  */
 #define BIG_LIST_SIZE 30
 
+extern void q_shuffle(struct list_head *head);
+
 /* Global variables */
 
 typedef struct {
@@ -1012,8 +1014,32 @@ static bool do_next(int argc, char *argv[])
     return q_show(0);
 }
 
+static bool do_shuffle(int argc, char *argv[])
+{
+    if (argc != 1) {
+        report(1, "%s takes no arguments", argv[0]);
+        return false;
+    }
+
+    if (!current || !current->q) {
+        report(3, "Warning: Calling shuffle on null queue");
+        return false;
+    }
+    error_check();
+
+    set_noallocate_mode(true);
+    if (exception_setup(true))
+        q_shuffle(current->q);
+    exception_cancel();
+
+    set_noallocate_mode(false);
+    q_show(3);
+    return !error_check();
+}
+
 static void console_init()
 {
+    ADD_COMMAND(shuffle, "Reorder all nodes in a random order", "");
     ADD_COMMAND(new, "Create new queue", "");
     ADD_COMMAND(free, "Delete queue", "");
     ADD_COMMAND(prev, "Switch to previous queue", "");
