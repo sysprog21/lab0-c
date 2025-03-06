f8f5cf25ba8741465e907e812a2bb83cd3bb4e7b
horseface <oloomb6@gmail.com>
2025-03-06 14:25:34 +0000

    Fix buffer overflow risk in q_remove_head
    Ensure strncpy does not write out of bounds by setting the last
    character to null terminator. Check for empty list before
    accessing the first entry.
    
    Change-Id: Ifc490e4d7d9789c1cc8bc04c8c3dbd023eb18422

--- queue.c
+++ queue.c
@@ -56,7 +56,16 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (list_empty(head)) {
+        return NULL;
+    }
+    element_t *entry = list_first_entry(head, element_t, list);
+    if (sp && entry->value) {
+        strncpy(sp, entry->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    list_del(&entry->list);
+    return entry;
 }
 
 /* Remove an element from tail of queue */
