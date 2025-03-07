1b1c0c57b5a78eeba8d09b431884f208b7d8c3a6
Lumynous <lumynou5.tw@gmail.com>
2025-03-08 02:28:12 +0800

    Introduce EditorConfig
    Originally, the indentation of the scripts was not unified: ones in
    'scripts/' used 2 spaces, and ones in '.ci/' used 4. Even though the
    former used 2 spaces, the style was not implemented well as some indents
    in the files were 4 spaces.
    
    This patch unifies the style of the scripts, and note it with an
    EditorConfig file. EditorConfig is used because it's supported
    out-of-the-box by many editors, helping the auto-indent functionality
    works.
    
    Change-Id: I41be8779ef5b0e94192c144bb48605625727963f

--- .ci/check-provisioning.sh
+++ .ci/check-provisioning.sh
@@ -2,7 +2,7 @@
 
 PROVISION_DIR=lab0-c-private
 if test -f $PROVISION_DIR/queue.c; then
-    cp -f $PROVISION_DIR/queue.c .
-    # Skip complexity checks
-    sed -i '/17:/d' scripts/driver.py
+  cp -f $PROVISION_DIR/queue.c .
+  # Skip complexity checks
+  sed -i '/17:/d' scripts/driver.py
 fi
